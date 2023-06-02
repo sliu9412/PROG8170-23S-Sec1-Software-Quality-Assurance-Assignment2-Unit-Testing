04f717eb61e074e87f326cb860bb9ad2921efef8
sliu9412 <sliu9412@conestogac.on.ca>
2023-06-02 11:32:10 -0700

    Isosceles Triangle Testing

--- TestClass/Class1.cs
+++ TestClass/Class1.cs
@@ -11,6 +11,7 @@ namespace TestClass
     [TestFixture]
     public class TriangleTest
     {
+        // Equilateral Triangle Testings
         [Test]
         public void EquilateralTriangleTest_Input5and5and5_OutputEquilateral()
         {
@@ -21,6 +22,46 @@ namespace TestClass
             string expected = "An EQUILATERAL triangle is formed";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
             
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Isosceles Triangle Testings
+        [Test]
+        public void IsoscelesTriangleTest_Input5and4and4_OutputIsosceles()
+        {
+            int firstSide = 5;
+            int secondSide = 4;
+            int thirdSide = 4;
+
+            string expected = "An ISOSCELES triangle is formed";
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IsoscelesTriangleTest_Input5and3and3_OutputIsosceles()
+        {
+            int firstSide = 5;
+            int secondSide = 3;
+            int thirdSide = 3;
+
+            string expected = "An ISOSCELES triangle is formed";
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void IsoscelesTriangleTest_Input4and5and5_OutputIsosceles()
+        {
+            int firstSide = 4;
+            int secondSide = 5;
+            int thirdSide = 5;
+
+            string expected = "An ISOSCELES triangle is formed";
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
             Assert.AreEqual(expected, actual);
         }
     }
