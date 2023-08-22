cca12a90c67860d1b9d2efee6a85a4e1a4ac49b9
Leo <sliu9412@conestogac.on.ca~>
2023-08-22 13:41:55 -0400

    add doc

--- Assignment2/TestClass/Class1.cs
+++ Assignment2/TestClass/Class1.cs
@@ -15,13 +15,14 @@ namespace TestClass
         [Test]
         public void EquilateralTriangleTest_Input5and5and5_OutputEquilateral()
         {
+            // Arrange
             int firstSide = 5;
             int secondSide = 5;
             int thirdSide = 5;
-
+            // Act
             string expected = "An EQUILATERAL triangle is formed";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-            
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
@@ -29,39 +30,42 @@ namespace TestClass
         [Test]
         public void IsoscelesTriangleTest_Input5and4and4_OutputIsosceles()
         {
+            // Arrange
             int firstSide = 5;
             int secondSide = 4;
             int thirdSide = 4;
-
+            // Act
             string expected = "An ISOSCELES triangle is formed";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void IsoscelesTriangleTest_Input5and3and3_OutputIsosceles()
         {
+            // Arrange
             int firstSide = 5;
             int secondSide = 3;
             int thirdSide = 3;
-
+            // Act
             string expected = "An ISOSCELES triangle is formed";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void IsoscelesTriangleTest_Input4and5and5_OutputIsosceles()
         {
+            // Arrange
             int firstSide = 4;
             int secondSide = 5;
             int thirdSide = 5;
-
+            // Act
             string expected = "An ISOSCELES triangle is formed";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
@@ -69,65 +73,70 @@ namespace TestClass
         [Test]
         public void ScaleneTriangleTest_Input5and4and3_OutputScalene()
         {
+            // Arrange
             int firstSide = 5;
             int secondSide = 4;
             int thirdSide = 3;
-
+            // Act
             string expected = "A SCALENE triangle is formed";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void ScaleneTriangleTest_Input6and4and3_OutputScalene()
         {
+            // Arrange
             int firstSide = 6;
             int secondSide = 4;
             int thirdSide = 3;
-
+            // Act
             string expected = "A SCALENE triangle is formed";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void ScaleneTriangleTest_Input6and5and3_OutputScalene()
         {
+            // Arrange
             int firstSide = 6;
             int secondSide = 5;
             int thirdSide = 3;
-
+            // Act
             string expected = "A SCALENE triangle is formed";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void ScaleneTriangleTest_Input6and5and4_OutputScalene()
         {
+            // Arrange
             int firstSide = 6;
             int secondSide = 5;
             int thirdSide = 4;
-
+            // Act
             string expected = "A SCALENE triangle is formed";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void ScaleneTriangleTest_Input7and5and4_OutputScalene()
         {
+            // Arrange
             int firstSide = 7;
             int secondSide = 5;
             int thirdSide = 4;
-
+            // Act
             string expected = "A SCALENE triangle is formed";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
@@ -135,39 +144,42 @@ namespace TestClass
         [Test]
         public void ZeroSidesTriangleTest_Input5and4and0_OutputInvalid()
         {
+            // Arrange
             int firstSide = 5;
             int secondSide = 4;
             int thirdSide = 0;
-
+            // Act
             string expected = "Invalid Triangle - at least one side is zero";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void ZeroSidesTriangleTest_Input5and0and0_OutputInvalid()
         {
+            // Arrange
             int firstSide = 5;
             int secondSide = 0;
             int thirdSide = 0;
-
+            // Act
             string expected = "Invalid Triangle - at least one side is zero";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void ZeroSidesTriangleTest_Input0and0and0_OutputInvalid()
         {
+            // Arrange
             int firstSide = 0;
             int secondSide = 0;
             int thirdSide = 0;
-
+            // Act
             string expected = "Invalid Triangle - at least one side is zero";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
@@ -175,39 +187,42 @@ namespace TestClass
         [Test]
         public void InvalidSidesTriangleTest_Input6and3and3_OutputInvalid()
         {
+            // Arrange
             int firstSide = 6;
             int secondSide = 3;
             int thirdSide = 3;
-
+            // Act
             string expected = "INVALID Triangle detected!!";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void InvalidSidesTriangleTest_Input8and4and3_OutputInvalid()
         {
+            // Arrange
             int firstSide = 8;
             int secondSide = 4;
             int thirdSide = 3;
-
+            // Act
             string expected = "INVALID Triangle detected!!";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
         public void InvalidSidesTriangleTest_Input5and5and10_OutputInvalid()
         {
+            // Arrange
             int firstSide = 5;
             int secondSide = 5;
             int thirdSide = 10;
-
+            // Act
             string expected = "INVALID Triangle detected!!";
             string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
-
+            // Assert
             Assert.AreEqual(expected, actual);
         }
     }
--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-# TriangleSolver
-Assignment 2
-sliu9412@conestogac.on.ca
-Siyu Liu
+# PROG8170-23S-Sec1-Software-Quality-Assurance-Assignment2-Unit-Testing
+
+For this assignment, we are going to develop unit tests for some C# (Class Library - .NET Framework). The requirements for this code are as follows:
+
+The application takes in three integers, each representing the sides of a triangle and based on the sides entered, shows what type of triangle could be formed – either Equilateral, Isosceles or Scalene.
+
+In doing so, the application makes use of the property that the sum of the two sides must be greater than the third - otherwise, an invalid response is returned.
+
+The application also checks to see if any side entered is a zero and if so, returns a particular error to this effect.
+
+## Requirements
+
+![](doc/screenshot/2023-08-22-13-29-33-image.png)
+
+![](doc/screenshot/2023-08-22-13-29-39-image.png)
+
+![](doc/screenshot/2023-08-22-13-29-44-image.png)
+
+## Project For Testing (C#)
+
+[TriangleSolver.zip](./doc/TriangleSolver.zip)
+
+## DOC
+
+![](doc/screenshot/2023-08-22-13-38-49-image.png)
+
+![](doc/screenshot/2023-08-22-13-39-59-image.png)
+
+![](doc/screenshot/2023-08-22-13-40-04-image.png)
